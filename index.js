6fb3f9cac65c56e80e5f32f11fd41210597d5e7e
Rogelio Orona <rogelio.orona@thomsonreuters.com>
2018-09-29 17:11:52 -0500

    Adds the initial state to the component

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import Media from './src/playlist/components/media'
 
 const app = document.getElementById('app')
 
-render(<Media />, app)
+render(<Media type="video" title="What is responsive design?" author="Steve Rogers" image="./images/covers/responsive.jpg" />, app)
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import './media.css'
 
 class Media extends Component {
+    state = {
+        author: 'Steve Rogers'
+    }
+
+    handleClick = (event) => {
+        this.setState({
+            author: 'Elon Musk'
+        })
+    }
+
     render() {
         const styles = {
             container: {
@@ -13,15 +24,27 @@ class Media extends Component {
         }
 
         return (
-            <div className="Media">
+            <div className="Media" onClick={this.handleClick}>
                 <div className="Media-cover">
-                    <img className="Media-image" src="./images/covers/bitcoin.jpg" alt="" width={260} height={160} />
-                    <h3 className="Media-title">Why learn React?</h3>
-                    <p className="Media-author">Steve Rogers</p>
+                    <img 
+                    className="Media-image" 
+                    src={this.props.image} 
+                    alt="" 
+                    width={260} 
+                    height={160} />
+                    <h3 className="Media-title">{this.props.title}</h3>
+                    <p className="Media-author">{this.state.author}</p>
                 </div>
             </div>
         )
     }
+}
+
+Media.propTypes = {
+    image: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string,
+    type: PropTypes.oneOf(['video', 'audio'])
 }
 
 export default Media
