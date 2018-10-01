e4cce81d962c116e4dcf53f12e6e3f35884ccfa7
Rogelio Orona <rogelio.orona@thomsonreuters.com>
2018-09-30 21:41:53 -0500

    Adds components for icons

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Media from './media'
 import './playlist.css'
+import Play from '../../icons/components/play'
+import Pause from '../../icons/components/pause'
+import Volume from '../../icons/components/volume'
+import FullScreen from '../../icons/components/full-screen'
 
 function Playlist (props) {    
     const playlist = props.data.categories[0].playlist
     console.log(props.data)
     return(
         <div className="Playlist">
+            <Play size={100} color="red" />
+            <Pause size={100} color="red" />
+            <Volume size={100} color="red" />
+            <FullScreen size={100} color="red" />
             {
                 playlist.map((item) => {
                     return <Media {...item} key={item.id}/>
