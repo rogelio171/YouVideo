5587d688475b026789cb89934121eb9d7cf429a1
Rogelio Orona <rogelio.orona@thomsonreuters.com>
2018-10-04 16:36:06 -0500

    Adds an error component

---
+++ src/error/components/regular-error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RegularError(props) {
+    return(
+        <h1 style={{color:'white'}}>An error has occurred</h1>
+    )
+}
+
+export default RegularError
--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -4,6 +4,7 @@ import Categories from '../../categories/components/categories'
 import Related from '../components/related'
 import ModalContainer from '../../widgets/containers/modal'
 import Modal from '../../widgets/components/modal'
+import HandleError from '../../error/containers/handle-error'
 
 class Home extends Component {
     state = {
@@ -24,21 +25,23 @@ class Home extends Component {
 
     render() {
         return(
-            <HomeLayout className="HomeLayout">
-                <Related />
-                <Categories 
-                categories={this.props.data.categories} 
-                handleOpenModal = {this.handleOpenModal} 
-                />
-                {
-                    this.state.modalVisible &&                
-                    <ModalContainer>
-                        <Modal handleClick={this.handleCloseModal}>
-                            <h1>This is a modal</h1>
-                        </Modal>
-                    </ModalContainer>
-                }
-            </HomeLayout>
+            <HandleError>                
+                <HomeLayout className="HomeLayout">
+                    <Related />
+                    <Categories 
+                    categories={this.props.data.categories} 
+                    handleOpenModal = {this.handleOpenModal} 
+                    />
+                    {
+                        this.state.modalVisible &&                
+                        <ModalContainer>
+                            <Modal handleClick={this.handleCloseModal}>
+                                <h1>This is a modal</h1>
+                            </Modal>
+                        </ModalContainer>
+                    }
+                </HomeLayout>
+            </HandleError>
         )
     }
 }
